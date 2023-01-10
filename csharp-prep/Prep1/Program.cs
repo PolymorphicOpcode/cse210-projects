570dc4d4b18c2b21c5bc8faad7aa051bdfa6fd28
idabro <>
2023-01-10 14:43:59 -0700

    First c# prep program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,14 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+class Program {
+    static void Main(string[] args) {
+        Console.Write("What is your first name? ");
+        // Could use string? for ambiguous whether variable exists
+        string FirstName = Console.ReadLine() ?? "";
+
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine() ?? "";
+
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
     }
 }
