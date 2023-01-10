2f41f5a54a95f6b96c881de57d736deb75902a47
idabro <>
2023-01-10 14:59:27 -0700

    Added C# prep 2 code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade as a percentage: ");
+        string Grade = Console.ReadLine();
+        int GradeAsInt = int.Parse(Grade);
+        char LetterGrade = ' ';
+        bool Passed = true;
+        if (GradeAsInt >= 90) {
+            LetterGrade = 'A';
+        }
+        else if (GradeAsInt >= 80) {
+            LetterGrade = 'B';
+        }
+        else if (GradeAsInt >= 70) {
+            LetterGrade = 'C';
+        }
+        else if (GradeAsInt >= 60) {
+            LetterGrade = 'D';
+        }
+        else if (GradeAsInt < 60) {
+            LetterGrade = 'F';
+            Passed = false;
+        }
+        else {
+            Console.WriteLine("An error has occured.");
+            Passed = false;
+        }
+        if (Passed) {
+            Console.WriteLine("Congratulations! You Passed.");
+        }
+        else {
+            Console.WriteLine("Better luck next time. Tutoring hours are 5pm-7pm");
+        }
+        Console.WriteLine($"Your grade as a letter is {LetterGrade}");
     }
 }
