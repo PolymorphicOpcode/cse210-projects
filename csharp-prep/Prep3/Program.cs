05782898a908055fdadc8fef5daac4fe1ffab2e7
idabro <>
2023-01-12 14:48:31 -0700

    added prep3 code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+
+        bool guessed = false;
+        while (!guessed) {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine() ?? "";
+            int randomuser;
+            int.TryParse(input, out randomuser);
+            if (randomuser == number) {
+                Console.WriteLine("Nice! You got it!");
+                guessed = true;
+                }
+            else if (randomuser > number) {
+                Console.WriteLine("Lower");
+            }
+            else if (randomuser < number) {
+                Console.WriteLine("Higher");
+            }
+            else {
+                Console.WriteLine("Error");
+            }
+        }
     }
 }
