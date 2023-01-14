56927dd785a32c40394f99d06323a73f2fb26ac3
idabro <>
2023-01-13 20:40:49 -0700

    added prep 5 file

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUsername() {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine() ?? "";
+        return name;
+    }
+
+    static int PromptUserNumber() {
+        Console.Write("Please enter your favorite number: ");
+        string favoriteNumber = Console.ReadLine();
+        int numberInt;
+        int.TryParse(favoriteNumber, out numberInt);
+        return numberInt;
+    }
+
+    static int SquareNumber(int num) {
+        return (num * num);
+    }
+    
+    static void DisplayResult(string username, int number) {
+        Console.WriteLine($"{username}, the square of your number is {number}");
+    }
+
+    static void Main(string[] args) {
+        DisplayWelcome();
+        string user = PromptUsername();
+        int number = PromptUserNumber();
+        DisplayResult(user, SquareNumber(number));
     }
 }
