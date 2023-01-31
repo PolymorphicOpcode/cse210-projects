85fc7dd8b1cf9239ceb4b5411472d95c98c488d5
idabro <>
2023-01-31 14:49:09 -0700

    added fraction (encapsulation) code

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,39 @@
+class Fraction {
+    private int _top;
+    private int _bottom;
+
+    public Fraction() {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber){
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction (int top, int bottom) {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    //Getters & Setters
+    public int GetTop() {
+        return _top;
+    }
+    private void SetTop(int top){
+        _top = top;
+    }
+
+    public int GetBottom() {
+        return _bottom;
+    }
+    private void SetBottom(int bottom){
+        _bottom = bottom;
+    }
+
+    public string GetFractionString() {
+        return $"{_top}/{_bottom}";
+    }
+    public double GetDecimalValue() {
+        return (double)_top / (double)_bottom;
+    }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        var account = new Account();
+
+        while(true) {
+            Console.Write("Press 1 to deposit money, press 2 to print your balance, and any other key to exit: ");
+            var input = Console.ReadLine() ?? "2";
+            if (input == "1") {
+                var amount = int.Parse(Console.ReadLine() ?? "");
+                account.Deposit(amount);
+            } else if (input == "2") {
+                Console.WriteLine($"Balance: {account.GetBalance()}");
+            } else {
+                break;
+            }
+        }
     }
 }
