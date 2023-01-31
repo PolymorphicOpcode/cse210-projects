135b4c28d7c0062902cede527085336e3fe910dd
idabro <>
2023-01-31 10:02:03 -0700

    added code comment to program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,5 +6,9 @@ class Program
     {
         Menu menu = new Menu();
         menu.Run();
+        /*I went above and beyond by learning a new data structure (datetime)
+        type to handle the date for the journal program, as well as 
+        utilizing chatgpt to explore concepts in c# that I hadn't 
+        learned yet and see the best practices for what I was doing*/
     }
 }
