0b4ab58fdd43dc9c9c0a8dc2934fd3d233c6fcad
idabro <>
2023-02-13 14:54:13 -0700

    fixed useropt by changing it to static field

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,23 +2,24 @@ using System;
 
 class Program {
     
-    private string _userOpt;
     // This program exceeds the requirements by not only fulfilling them, but being optimized code. I asked an AI to improve it, so I learned how string
     // constructors worked, and how the random function works.
+    public static string userOpt = "";
+
     static void Main(string[] args)
     {
         Reference myreference = new Reference("John", 3, 16);
         Scripture myscripture = new Scripture(myreference, "For God so loved the world, that he gave his only begotten Son; that whosover believes in him should not perish but have eternal life.");
         Console.WriteLine("Memorize then press enter");
         myscripture.Display();
-        _userOpt = Console.ReadLine() ?? "";
+        userOpt = Console.ReadLine() ?? "";
         
-        while(!myscripture.IsCompletelyHidden() && _userOpt != "quit") {
+        while(!myscripture.IsCompletelyHidden() && userOpt != "quit") {
             myscripture.HideWords();
             Console.Clear();
             Console.WriteLine("Memorize then press enter");
             myscripture.Display();
-            _userOpt = Console.ReadLine() ?? "";
+            userOpt = Console.ReadLine() ?? "";
         }
     }
     }
